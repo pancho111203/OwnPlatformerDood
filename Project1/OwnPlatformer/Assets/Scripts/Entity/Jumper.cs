7565d1a50fbe3b4ec50c56a5674233f8874e78ef
pancho111203 <pancho_111203@hotmail.com>
2015-03-07 16:05:19 +0100

    first commit project

---
+++ Project1/OwnPlatformer/Assets/Scripts/Entity/Jumper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jumper : MonoBehaviour {
+	public float force = 1500.0f;
+
+
+	// NOT WORKING
+
+	void OnCollisionEnter (Collision collision){
+		Debug.Log(collision.gameObject.tag+"ssdsd");
+		if(collision.gameObject.tag == "Player"){
+			Debug.Log("TSER");
+			Rigidbody2D rig =  collision.gameObject.GetComponent<Rigidbody2D>();
+			rig.AddForce(Vector3.up*force);
+		}
+	}
+	void OnTriggerEnter (Collider collision){
+		Debug.Log(collision.gameObject.tag+"ssdsd");
+		if(collision.gameObject.tag == "Player"){
+			Debug.Log("TSER");
+			Rigidbody2D rig =  collision.gameObject.GetComponent<Rigidbody2D>();
+			rig.AddForce(Vector3.up*force);
+		}
+	}
+}
---
+++ Project1/OwnPlatformer/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+	public static GameMaster gm;
+
+
+	// Use this for initialization
+	void Start () {
+		// initialize singleton
+		if(gm=null){
+			gm = gameObject.GetComponent<GameMaster>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
